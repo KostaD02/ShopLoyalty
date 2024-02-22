ec50783b0a9f70214fed572de4fe091db7965ef8
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-02-22 16:06:10 +0400

    feat: home and notfound base components & routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home.component'),
+    title: 'მთავარი',
+  },
+  {
+    path: '404',
+    loadComponent: () => import('./features/not-found/not-found.component'),
+    title: 'გვერდი ვერ მოიძებნა',
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '404',
+  },
+];
