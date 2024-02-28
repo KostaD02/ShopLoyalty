0fccf9d700e99eb7d98ff3e9175189347462bf78
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-02-28 16:16:23 +0400

    feat(client): qr code reader

--- client/src/app/features/scanner/scanner.component.ts
+++ client/src/app/features/scanner/scanner.component.ts
@@ -1,15 +1,17 @@
-import { isPlatformBrowser } from '@angular/common';
+import { AsyncPipe, isPlatformBrowser } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   PLATFORM_ID,
   inject,
 } from '@angular/core';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-scanner',
   standalone: true,
-  imports: [],
+  imports: [ZXingScannerModule, AsyncPipe],
   templateUrl: './scanner.component.html',
   styleUrl: './scanner.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,4 +19,15 @@ import {
 export default class ScannerComponent {
   private readonly platform = inject(PLATFORM_ID);
   readonly isBrowser = isPlatformBrowser(this.platform);
+
+  readonly agreed$ = new BehaviorSubject<boolean>(false);
+  readonly isAgreed$ = this.agreed$.asObservable();
+
+  onSuccess(result: string) {
+    console.log(result);
+  }
+
+  onPermission(agreed: boolean) {
+    this.agreed$.next(agreed);
+  }
 }
