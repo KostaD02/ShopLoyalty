51143283ee30c437a4649bd18d0b003bed7620af
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-03-03 12:04:18 +0400

    chore: update API configs

--- client/src/app/shared/consts/api.ts
+++ client/src/app/shared/consts/api.ts
@@ -1,8 +1,9 @@
-export const FRONTEND_ENDPOINT_DOMAIN = 'localhost:4200';
-export const FRONTEND_ENDPOINT = `http://${FRONTEND_ENDPOINT_DOMAIN}`;
+export const FRONTEND_ENDPOINT_DOMAIN =
+  'shoployalty.konstantinedatunishvili.com';
+export const FRONTEND_ENDPOINT = `https://${FRONTEND_ENDPOINT_DOMAIN}`;
 
-export const BACKEND_ENDPOINT_DOMAIN = 'localhost:3000';
-export const BACKEND_ENDPOINT = `http://${BACKEND_ENDPOINT_DOMAIN}`;
+export const DEFAULT_IMAGE_URL = `${FRONTEND_ENDPOINT}/assets/default_image.jpg`;
 
-export const DEFAULT_IMAGE_URL =
-  'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=';
+export const BACKEND_ENDPOINT_DOMAIN =
+  'backend-shoployalty.konstantinedatunishvili.com';
+export const BACKEND_ENDPOINT = `https://${BACKEND_ENDPOINT_DOMAIN}`;
